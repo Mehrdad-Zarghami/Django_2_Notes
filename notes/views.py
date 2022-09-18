2cebad105f405f237fadccd19f95f59dabf8770c
Mehrdad <mehrdad.zarghami69@gmail.com>
2022-09-18 19:34:55 +0430

    reach to something that works; for loop in django templates

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Note
 
-# Create your views here.
+
+def notes_list_view(request):
+    notes = Note.objects.all()
+    context = {
+        'list_of_notes': notes,
+    }
+    return render(request, 'notes/notes_list.html', context)
+
